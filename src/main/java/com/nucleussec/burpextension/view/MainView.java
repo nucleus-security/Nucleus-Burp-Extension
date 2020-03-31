9e4293ae77549874ebd5300ae609600dce24dd4a
Eredis Gutierrez <egutierrez@nucleussec.com>
2020-03-31 11:44:23 -0400

    Added ScrollPane

--- src/main/java/com/nucleussec/burpextension/view/MainView.java
+++ src/main/java/com/nucleussec/burpextension/view/MainView.java
@@ -193,52 +193,29 @@ public class MainView extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtNucleusInstanceURL = new javax.swing.JTextField();
+        pwApiKey = new javax.swing.JPasswordField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         cbProjects = new javax.swing.JComboBox<>();
-        btnPushToNucleus = new javax.swing.JButton();
-        jProgressBar = new javax.swing.JProgressBar();
-        jLabel4 = new javax.swing.JLabel();
         btnSyncWithNucleus = new javax.swing.JButton();
-        pwApiKey = new javax.swing.JPasswordField();
-        jLabel5 = new javax.swing.JLabel();
-        txtNucleusInstanceURL = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        btnRefreshScanURLs = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         listScanUrls = new javax.swing.JList<>();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        btnRefreshScanURLs = new javax.swing.JButton();
-
-        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btnPushToNucleus = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jProgressBar = new javax.swing.JProgressBar();
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(204, 102, 0));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel1.setText("Configure Nucleus Server");
-
-        jLabel2.setText("Nucleus API Key:");
-
-        jLabel3.setText("Project:");
-
-        btnPushToNucleus.setText("Upload Issues");
-        btnPushToNucleus.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnPushToNucleusActionPerformed(evt);
-            }
-        });
-
-        jLabel4.setText("Upload Status:");
-
-        btnSyncWithNucleus.setText("Sync with Nucleus");
-        btnSyncWithNucleus.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSyncWithNucleusActionPerformed(evt);
-            }
-        });
-
-        pwApiKey.setText("jPasswordField1");
 
         jLabel5.setText("Nucleus Instance URL:");
 
@@ -248,15 +225,25 @@ public class MainView extends javax.swing.JPanel {
             }
         });
 
-        listScanUrls.setModel(new DefaultListModel<String>());
-        jScrollPane1.setViewportView(listScanUrls);
+        pwApiKey.setText("jPasswordField1");
 
-        jLabel6.setText("Select Scan URLs to Include:");
+        jLabel2.setText("Nucleus API Key:");
+
+        jLabel3.setText("Project:");
+
+        btnSyncWithNucleus.setText("Sync with Nucleus");
+        btnSyncWithNucleus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSyncWithNucleusActionPerformed(evt);
+            }
+        });
 
         jLabel7.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(204, 102, 0));
         jLabel7.setText("Upload Results to Nucleus");
         jLabel7.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+
+        jLabel6.setText("Select Scan URLs to Include:");
 
         btnRefreshScanURLs.setIcon(new javax.swing.ImageIcon(getClass().getResource("/refresh.png"))); // NOI18N
         btnRefreshScanURLs.setBorderPainted(false);
@@ -267,88 +254,102 @@ public class MainView extends javax.swing.JPanel {
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(238, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        listScanUrls.setModel(new DefaultListModel<String>());
+        jScrollPane1.setViewportView(listScanUrls);
+
+        btnPushToNucleus.setText("Upload Issues");
+        btnPushToNucleus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPushToNucleusActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Upload Status:");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap(239, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 907, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
                             .addComponent(jLabel2)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(pwApiKey, javax.swing.GroupLayout.PREFERRED_SIZE, 665, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(btnSyncWithNucleus))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createSequentialGroup()
                             .addComponent(jLabel5)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(txtNucleusInstanceURL, javax.swing.GroupLayout.PREFERRED_SIZE, 776, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 908, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnRefreshScanURLs, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
                             .addComponent(jLabel3)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(cbProjects, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createSequentialGroup()
                             .addComponent(btnPushToNucleus, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
                             .addComponent(jLabel4)
                             .addGap(18, 18, 18)
                             .addComponent(jProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 568, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)))
-                .addContainerGap(238, Short.MAX_VALUE))
+                .addContainerGap(239, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(35, 35, 35)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(txtNucleusInstanceURL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(pwApiKey, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnSyncWithNucleus))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cbProjects, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addGap(109, 109, 109)
                 .addComponent(jLabel7)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(btnRefreshScanURLs, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(41, 41, 41)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jLabel4)
                         .addComponent(btnPushToNucleus)))
                 .addGap(721, 721, 721))
         );
 
+        jScrollPane2.setViewportView(jPanel2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane2)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane2)
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -364,13 +365,6 @@ public class MainView extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_btnSyncWithNucleusActionPerformed
 
-    private void btnPushToNucleusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPushToNucleusActionPerformed
-        String errors = "";
-        if (prefs.get("instance_url", "").isEmpty() || prefs.get("x-apikey", "").isEmpty() || txtNucleusInstanceURL.getText().isEmpty() || pwApiKey.getPassword().length == 0 | listScanUrls.isSelectionEmpty()) {
-            JOptionPane.showMessageDialog(this, "Some fields appear to be empty or not selected. Please fill in or select these fields and try again.", "Error has occured", JOptionPane.ERROR_MESSAGE);
-        } else pushToNucleus();
-    }//GEN-LAST:event_btnPushToNucleusActionPerformed
-
     private void btnRefreshScanURLsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRefreshScanURLsActionPerformed
         btnRefreshScanURLs.setIcon(new javax.swing.ImageIcon(getClass().getResource("/refresh-sync.png"))); // NOI18N
         populateScanUrlsList();
@@ -381,6 +375,13 @@ public class MainView extends javax.swing.JPanel {
             }
         }, 500);
     }//GEN-LAST:event_btnRefreshScanURLsActionPerformed
+
+    private void btnPushToNucleusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPushToNucleusActionPerformed
+        String errors = "";
+        if (prefs.get("instance_url", "").isEmpty() || prefs.get("x-apikey", "").isEmpty() || txtNucleusInstanceURL.getText().isEmpty() || pwApiKey.getPassword().length == 0 | listScanUrls.isSelectionEmpty()) {
+            JOptionPane.showMessageDialog(this, "Some fields appear to be empty or not selected. Please fill in or select these fields and try again.", "Error has occured", JOptionPane.ERROR_MESSAGE);
+        } else pushToNucleus();
+    }//GEN-LAST:event_btnPushToNucleusActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -395,9 +396,10 @@ public class MainView extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JProgressBar jProgressBar;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JList<String> listScanUrls;
     private javax.swing.JPasswordField pwApiKey;
     private javax.swing.JTextField txtNucleusInstanceURL;
